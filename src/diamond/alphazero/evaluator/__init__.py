d6428b615b0213fb5b02707a959751239193352d
Doh <dko038@uottawa.ca>
2026-08-25 03:34:25 +0900

    refactor(python-zero): retire training and release routes

--- src/diamond/alphazero/evaluator/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Framework-neutral evaluator contract and implementations."""
-
-from .base import EvalRequest, EvalResult, Evaluator
-from .dummy import DummyEvaluator
-
-__all__ = ["DummyEvaluator", "EvalRequest", "EvalResult", "Evaluator"]
--- src/diamond/alphazero/network/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Soo/Min models over shared neutral graph-network components."""
-
-from .min import MinModel
-from .soo import SooModel
-
-__all__ = ["MinModel", "SooModel"]
